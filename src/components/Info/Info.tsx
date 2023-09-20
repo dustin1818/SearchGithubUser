399f86d2c3f8a6c679fac8758df3dee1f294b0b5
dustin1818 <dustinamoda18@gmail.com>
2023-09-20 22:06:23 +0800

    update styling make it tablet/mobile responsive

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -10,7 +10,7 @@ export default function Info() {
   return (
     <div
       className={clsx(
-        "container__effect shadow-[0px_50px_-25px_rgba(0, 0, 0.10)] absolute inset-x-0 top-10 z-10 mx-auto flex h-[195px] w-full max-w-[1210px] translate-y-[16.7rem] items-start justify-start gap-14 rounded-[1.5rem] bg-white p-[37px] text-[#2C2C2C]"
+        "flex flex-col gap-5 h-auto max-w-[90%]  p-7 md:container__effect md:shadow-[0px_50px_-25px_rgba(0, 0, 0.10)] absolute inset-x-0 top-10 z-10 mx-auto md:flex md:flex-row w-full md:max-w-[1210px] translate-y-[16.7rem] md:items-start md:justify-start md:gap-14 rounded-[1.5rem] bg-white md:p-[37px] text-[#2C2C2C]"
       )}>
       <div className="flex flex-col gap-3">
         <p className="font-bold uppercase tracking-[1.75px] opacity-[0.5] ">
@@ -19,7 +19,7 @@ export default function Info() {
         <h2 className="text-[26px] font-bold">{state.ipAddress}</h2>
       </div>
 
-      <div className=" h-[75px] w-[1px] bg-black opacity-[0.15]"></div>
+      <div className=" h-[1px] w-full  md:h-[75px] md:w-[1px] bg-black opacity-[0.15]"></div>
       <div className="flex flex-col gap-3">
         <p className="font-bold uppercase tracking-[1.75px] opacity-[0.5] ">
           Location
@@ -28,7 +28,7 @@ export default function Info() {
           {state.city}, {state.region} 10001
         </h2>
       </div>
-      <div className=" h-[75px] w-[1px] bg-black opacity-[0.15]"></div>
+      <div className=" h-[1px] w-full  md:h-[75px] md:w-[1px] bg-black opacity-[0.15]"></div>
       <div className="flex flex-col gap-3">
         <p className="font-bold uppercase tracking-[1.75px] opacity-[0.5] ">
           Timezone
@@ -36,7 +36,7 @@ export default function Info() {
         <h2 className="text-[26px] font-bold">UTC {state.timezone}</h2>
       </div>
 
-      <div className=" h-[75px] w-[1px] bg-black opacity-[0.15]"></div>
+      <div className=" h-[1px] w-full  md:h-[75px] md:w-[1px] bg-black opacity-[0.15]"></div>
       <div className="flex flex-col gap-3">
         <p className="font-bold uppercase tracking-[1.75px] opacity-[0.5] ">
           ISP
