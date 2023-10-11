d07e207ca616399cf569dd9f63adcf920b8ec89f
dustin1818 <dustinamoda18@gmail.com>
2023-10-11 18:11:53 +0800

    add grid responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import Header from "./components/Header/Header";
 import InfoCard from "./components/Info/InfoCard";
 import Maps from "./components/Map/Maps";
 import { MapProvider } from "./context/MapContext";
+import { useState } from "react";
 
 export default function App() {
+  const [errors, setErrors] = useState("");
+
   return (
     <MapProvider>
       <section
         className={clsx(
           "relative grid min-h-screen grid-cols-[1fr] grid-rows-[28rem_1fr]"
         )}>
-        <Header />
+        <Header errors={errors} setErrors={setErrors}  />
         <InfoCard />
         <Maps />
       </section>
